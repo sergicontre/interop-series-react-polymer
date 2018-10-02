084df501982e97ef436a6a8f8e6500604de1b450
sergicontre <sergicontre@users.noreply.github.com>
2018-10-02 15:47:01 +0200

    fix(): url image of vue

--- src/App.js
+++ src/App.js
@@ -7,32 +7,31 @@ class App extends Component {
 
   constructor(props){
     super(props);
-    this.name = 'Mariano'
+    this.name = 'React'
     this.description = 'Developer'
     this.handleLikeEvent = this.handleLikeEvent.bind(this);
   }
 
   handleLikeEvent() {
-    //TODO: like
-    console.log('===>')
+    console.log('event like from web component');
   }
 
   handleNopeEvent() {
-    //TODO: nope
+    console.log('event nope from web component');
   }
 
   componentDidMount() {
     // When the component is mounted, add your DOM listener to the "nv" elem.
     // (The "nv" elem is assigned in the render function.)
-    //document.querySelector('#tinder').addEventListener("like", this.handleLikeEvent);
-    //document.querySelector('#tinder').addEventListener("nope", this.handleNopeEvent);
+    document.querySelector('#tinder').addEventListener("like", this.handleLikeEvent);
+    document.querySelector('#tinder').addEventListener("nope", this.handleNopeEvent);
   }
 
   render() {
     
     return (
       <div className="App">
-       <tinder-item id="tinder" avatar="https://ivi.ie/wp-content/uploads/2016/04/default-avatar.png" name={this.name} description={this.description} onLike={this.handleLikeEvent}></tinder-item>
+       <tinder-item id="tinder" avatar="https://github.com/sergicontre/interop-series-react-polymer/blob/master/public/react.png?raw=true" name={this.name} description={this.description} onLike={this.handleLikeEvent}></tinder-item>
       </div>
     );
   }
